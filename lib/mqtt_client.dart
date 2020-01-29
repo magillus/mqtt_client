644907bfbccd7b762a4184ca827a807259a2c733
Mateusz Perlak <mati.akip@gmail.com>
2020-01-29 17:29:18 -0600

    small export fix

--- lib/mqtt_client.dart
+++ lib/mqtt_client.dart
@@ -15,8 +15,6 @@ import 'package:typed_data/typed_data.dart' as typed;
 import 'package:event_bus/event_bus.dart' as events;
 import 'src/observable/observable.dart' as observe;
 
-export 'src/stub/mqtt_client_stub.dart';
-
 /// The mqtt_client package exported interface
 part 'src/mqtt_client.dart';
 
