e376d5d640ae3435452ac6d98d7628cbf20b4786
Mateusz Perlak <mati.akip@gmail.com>
2020-01-31 03:19:35 -0600

    Added optional websocket for createClientWithPort method

--- lib/src/stub/mqtt_browser_stub.dart
+++ lib/src/stub/mqtt_browser_stub.dart
@@ -2,5 +2,6 @@ import '../../mqtt_client.dart';
 import '../../mqtt_browser_client.dart';
 
 MqttClient createClientWithPort(
-        String server, String clientIdentifier, int port) =>
-    MqttBrowserClient.withPort('ws://$server/ws', clientIdentifier, port);
+        String server, String clientIdentifier, int port, {int wsPort}) =>
+    MqttBrowserClient.withPort(
+        'ws://$server/ws', clientIdentifier, wsPort ?? port);
